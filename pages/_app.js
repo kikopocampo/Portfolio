987af7dfccc10f145a75d35697c1eeae719fb0bf
Francisco <kikopocampo@gmail.com>
2023-01-24 16:51:37 -0700

    Added initial style

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Rubik, Nunito } from "@next/font/google";
+
+const rubik = Rubik({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={nunito.className}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
