0a5cf7622c1572f7c06cfbd64f443825dab1c6f4
Francisco <kikopocampo@gmail.com>
2023-01-26 08:55:29 -0700

    prep for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+  purge: [
+    // "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./Components/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    // "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
