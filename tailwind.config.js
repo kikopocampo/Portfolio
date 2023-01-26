25258365e9e653146af2f23ac433abf8db1446c2
Francisco <kikopocampo@gmail.com>
2023-01-26 09:05:05 -0700

    fix style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    // "./app/**/*.{js,ts,jsx,tsx}",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./Components/**/*.{js,ts,jsx,tsx}",
-    // "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,svelte,ts}",
   ],
   theme: {
     extend: {},
